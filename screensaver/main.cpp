6a7ef884d48c4fd1f5405aa2e4b2009e38e49e6f
ultimatum424 <ultimatum424@yandex.ru>
2015-12-02 14:58:05 +0300

    saver 1.7

--- screensaver/main.cpp
+++ screensaver/main.cpp
@@ -24,107 +24,106 @@ struct StageStruct
 	int rotation;
 };
 
-void Draw(RenderWindow& window, RectangleStruct Rect[QUANTITY])
+void Draw(RenderWindow& window, RectangleStruct rect[QUANTITY])
 {
 	for (int i = 0; i < QUANTITY; i++)
 	{
-		window.draw(Rect[i].square);
+		window.draw(rect[i].square);
 	}
 	window.display();
 	window.clear(sf::Color::White);
 }
-void InitializationStage(StageStruct& Stage)
+void InitializationStage(StageStruct& stage)
 {
-	Stage.color = 1;
-	Stage.size = 1;
-	Stage.move = 1;
-	Stage.rotation = 1;
+	stage.color = 1;
+	stage.size = 1;
+	stage.move = 1;
+	stage.rotation = 1;
 }
-void InitializationSecret(SecreteStruct& Secret)
+void InitializationSecret(SecreteStruct& secret)
 {
-	Secret.color = 1;
-	Secret.move = 1;
-	Secret.rotation = 1;
-	Secret.size = 1;
+	secret.color = 1;
+	secret.move = 1;
+	secret.rotation = 1;
+	secret.size = 1;
 }
-void InitializationSqure(RectangleStruct(& Rect)[QUANTITY])
+void InitializationSqure(RectangleStruct(&rect)[QUANTITY])
 {
 	for (int i = 0; i < QUANTITY; i++)
 	{
-		Rect[i].square.setSize(sf::Vector2f(SQURE_SIEZE, SQURE_SIEZE));
-		Rect[i].square.setFillColor(Color(255, 0, 0));
-		Rect[i].square.setPosition(50 * i, 50);
+		rect[i].square.setSize(sf::Vector2f(SQURE_SIEZE, SQURE_SIEZE));
+		rect[i].square.setFillColor(Color(255, 0, 0));
+		rect[i].square.setPosition(50 * i, 50);
 	}	
 }
-void ChangeColor(RectangleStruct(&Rect)[QUANTITY], int i, float time)
+void ChangeColor(RectangleStruct(&rect)[QUANTITY], int i, float time)
 {
 	
-	Rect[0].square.setFillColor(Color(255, i* 1 * time, 0));
-	Rect[1].square.setFillColor(Color(0, i * 1 * time, i* 1 * time));
-	Rect[2].square.setFillColor(Color(0, i* 1 * time, 0));
-	Rect[3].square.setFillColor(Color(i* 1 * time, 0, 0));
-	Rect[4].square.setFillColor(Color(255, 0, i * 1 * time));
+	rect[0].square.setFillColor(Color(255, i* 1 * time, 0));
+	rect[1].square.setFillColor(Color(0, i * 1 * time, i* 1 * time));
+	rect[2].square.setFillColor(Color(0, i* 1 * time, 0));
+	rect[3].square.setFillColor(Color(i* 1 * time, 0, 0));
+	rect[4].square.setFillColor(Color(255, 0, i * 1 * time));
 }
-void ChangeSize(RectangleStruct(&Rect)[QUANTITY], float j, float time)
+void ChangeSize(RectangleStruct(&rect)[QUANTITY], float j, float time)
 {
 	for (int i = 0; i < QUANTITY; i++)
 	{
-		Rect[i].square.setScale(j, j);
+		rect[i].square.setScale(j, j);
 	}
 }
-void StageSelect(StageStruct& Stage, int i, float j, RectangleStruct Rect[QUANTITY])
+void StageSelect(StageStruct& stage, int i, float j, RectangleStruct rect[QUANTITY])
 {
-	if (i == 256) Stage.color = -1;
-	if (i == 0) Stage.color = 1;
-	if (j >= 2) { Stage.size = -1; }
-	if (j <= 1) { Stage.size =  1; }
-	if ((Rect[2].square.getPosition().x <= 150) && (Rect[2].square.getPosition().y <= 100)) Stage.move = 1;
-	if ((Rect[2].square.getPosition().x >= WINDOW_X - 200) && (Rect[2].square.getPosition().y <= 100)) Stage.move = 2;
-	if ((Rect[2].square.getPosition().x >= WINDOW_X - 200) && (Rect[2].square.getPosition().y >= WINDOW_X - 100)) Stage.move = 3;
-	if ((Rect[2].square.getPosition().x <= 150) && (Rect[2].square.getPosition().y >= WINDOW_X - 100)) Stage.move = 4;
+	if (i == 256) stage.color = -1;
+	if (i == 0) stage.color = 1;
+	if (j >= 2) { stage.size = -1; }
+	if (j <= 1) { stage.size =  1; }
+	if ((rect[2].square.getPosition().x <= 150) && (rect[2].square.getPosition().y <= 100)) stage.move = 1;
+	if ((rect[2].square.getPosition().x >= WINDOW_X - 200) && (rect[2].square.getPosition().y <= 100)) stage.move = 2;
+	if ((rect[2].square.getPosition().x >= WINDOW_X - 200) && (rect[2].square.getPosition().y >= WINDOW_X - 100)) stage.move = 3;
+	if ((rect[2].square.getPosition().x <= 150) && (rect[2].square.getPosition().y >= WINDOW_X - 100)) stage.move = 4;
 
 }
-void Move(RectangleStruct(&Rect)[QUANTITY], Vector2f speed_move)
+void Move(RectangleStruct(&rect)[QUANTITY], Vector2f speed_move)
 {
 	for (int i = 0; i < QUANTITY; i++)
 	{
-		Rect[i].square.move(speed_move.x, speed_move.y);
+		rect[i].square.move(speed_move.x, speed_move.y);
 	}
 }
-void Rotation(RectangleStruct(&Rect)[QUANTITY], float time)
+void Rotation(RectangleStruct(&rect)[QUANTITY], float time)
 {
 	for (int i = 0; i < QUANTITY; i++)
 	{
-		Rect[i].square.setRotation(Rect[i].square.getRotation() + (1 * time));
+		rect[i].square.setRotation(rect[i].square.getRotation() + (1 * time));
 		
 	}
 }
-void SceneChange(StageStruct Stage, int& i_color, float& i_size, Vector2f& speed_move, float& time)
+void SceneChange(StageStruct stage, int& i_color, float& i_size, Vector2f& speed_move, float& time)
 {
-	if (Stage.color == 1) i_color++;
-	if (Stage.color == -1) i_color--;
-	if (Stage.size == 1) i_size = i_size + 0.1 * time / 100;
-	if (Stage.size == -1) i_size = i_size - 0.1 * time / 100;
-	if (Stage.move == 1) { speed_move.x = SPEED; speed_move.y = 0; }
-	if (Stage.move == 2) { speed_move.x = 0; speed_move.y = SPEED; }
-	if (Stage.move == 3) { speed_move.x = -SPEED; speed_move.y = 0; }
-	if (Stage.move == 4) { speed_move.x = 0; speed_move.y = -SPEED; }
+	if (stage.color == 1) i_color++;
+	if (stage.color == -1) i_color--;
+	if (stage.size == 1) i_size = i_size + 0.1 * time / 100;
+	if (stage.size == -1) i_size = i_size - 0.1 * time / 100;
+	if (stage.move == 1) { speed_move.x = SPEED; speed_move.y = 0; }
+	if (stage.move == 2) { speed_move.x = 0; speed_move.y = SPEED; }
+	if (stage.move == 3) { speed_move.x = -SPEED; speed_move.y = 0; }
+	if (stage.move == 4) { speed_move.x = 0; speed_move.y = -SPEED; }
+
+	
 }
-int main()
+void RunProgram(RenderWindow& window)
 {
-	struct RectangleStruct Rect[QUANTITY] = {};
-	struct SecreteStruct Secret;
-	struct StageStruct Stage;
-	sf::ContextSettings settings;
-	settings.antialiasingLevel = 8;
-	sf::RenderWindow window(sf::VideoMode(WINDOW_X, WINDOW_Y), "ScreenSaver", sf::Style::Default, settings);
-	InitializationSqure(Rect);
+	RectangleStruct rect[QUANTITY] = {};
+	SecreteStruct secret;
+	StageStruct stage;
+	InitializationSqure(rect);
 	Clock clock;
 	Clock change_clock;
 	srand(time(NULL));
 
-	InitializationStage(Stage);
-	int secret = 1;
+	InitializationStage(stage);
+	int secret_stage = 1;
 	int i_color = 0;
 	int num_turns = 0;
 	float i_size = 1;
@@ -133,7 +132,7 @@ int main()
 	speed_move.x = SPEED;
 	speed_move.y = SPEED;
 
-	InitializationSecret(Secret);
+	InitializationSecret(secret);
 
 	change_clock.restart();
 	while (window.isOpen())
@@ -146,10 +145,10 @@ int main()
 		if (time_for_change >= TIME)
 		{
 			change_clock.restart();
-			Secret.color = rand() % 2;
-			Secret.move = rand() % 2;
-			Secret.rotation = rand() % 2;
-			Secret.size = rand() % 2;
+			secret.color = rand() % 2;
+			secret.move = rand() % 2;
+			secret.rotation = rand() % 2;
+			secret.size = rand() % 2;
 		}
 		while (window.pollEvent(event))
 		{
@@ -157,15 +156,23 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		StageSelect(Stage, i_color, i_size, Rect);
-		SceneChange(Stage, i_color, i_size, speed_move, time);
-		
-		if (Secret.rotation == 1) Rotation(Rect, time);
-		if (Secret.color == 1) ChangeColor(Rect, i_color, time);
-		if (Secret.size == 1) ChangeSize(Rect, i_size, time);
-		if (Secret.move == 1) Move(Rect, speed_move);
+		StageSelect(stage, i_color, i_size, rect);
+		SceneChange(stage, i_color, i_size, speed_move, time);
 
-		Draw(window, Rect);
+		if (secret.rotation == 1) Rotation(rect, time);
+		if (secret.color == 1) ChangeColor(rect, i_color, time);
+		if (secret.size == 1) ChangeSize(rect, i_size, time);
+		if (secret.move == 1) Move(rect, speed_move);
+
+		Draw(window, rect);
 	}
+}
+int main()
+{
+	
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode(WINDOW_X, WINDOW_Y), "ScreenSaver", sf::Style::Default, settings);
+	RunProgram(window);
 	return 0;
 }
